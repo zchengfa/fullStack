5e70e8a4ac3a7f11cac99a53cc3c9e2042049130
zchengfa <1522977814@qq.com>
2021-09-14 11:32:36 +0800

    delete useless files

--- admin/src/common/utils.js
+++ admin/src/common/utils.js
@@ -1,7 +1,1 @@
 export const URL = 'http:'+ '//' + window.location.host.toString().split(':')[0] + ':3000'
-
-import {verify} from 'jsonwebtoken'
-export function verifyToken (token,callback) {
-
-    verify(token,'administrator',callback)
-}

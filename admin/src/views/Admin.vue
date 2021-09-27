c3806900ae0b6a5dff488eecc4faf1d8d0af4a49
zchengfa <1522977814@qq.com>
2021-09-27 11:59:23 +0800

    add table

--- admin/src/views/Admin.vue
+++ admin/src/views/Admin.vue
@@ -3,8 +3,13 @@
     <el-header class="admin-header">{{welcome.title}}</el-header>
     <el-main class="admin-main">
       <el-tabs tab-position="left" class="left-tabs">
-        <el-tab-pane v-for="(item,index) in leftTab.tabMenu" :key="index" :label="item"></el-tab-pane>
+        <el-tab-pane v-for="(item,index) in leftTab.tabMenu" :key="index" :label="item">
+          <el-table :data="tableLogic.tableData">
+            <el-table-column v-for="(header,headerIndex) in tableLogic.tableHeader" :key="header" :prop="returnTableProp(headerIndex)"  :label="header"></el-table-column>
+          </el-table>
+        </el-tab-pane>
       </el-tabs>
+
     </el-main>
   </el-container>
 </template>
@@ -48,14 +53,53 @@ export default defineComponent({
      * @param tabMenu 存储左侧标签栏菜单标题数据
      */
     let leftTab = reactive({
-      tabMenu:['商品管理','订单管理','会员管理','营销管理','数据统计']
+      tabMenu:<object>['商品管理','订单管理','会员管理','营销管理','数据统计']
     })
 
+    /**
+     * @param tableData 存储表格内的数据
+     */
+
+    let tableLogic = reactive({
+      tableHeader:<Object>['商品描述', '商品图片', '数量', '价格'],
+      tableData:[
+        {
+          title:<string>'我是商品标题',
+          image:<string>'我是商品图片链接',
+          count:<number>10,
+          price:<number>18
+        },
+        {
+          title:<string>'我是商品标题2',
+          image:<string>'我是商品图片链接2',
+          count:<number>102,
+          price:<number>182
+        }
+      ]
+    })
+    function returnTableProp(index:number) {
+      switch (index){
+        case 0:
+          return 'title';
+
+        case 1:
+          return 'image'
+
+        case 2:
+          return 'count'
+
+        case 3:
+          return 'price'
+
+      }
+    }
 
     return {
       welcome,
       checkIsLogin,
-      leftTab
+      leftTab,
+      tableLogic,
+      returnTableProp
     }
   }
 

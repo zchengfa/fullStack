bcd6a345d34878e5b5b8387358b269ebff2ebb88
zchengfa <1522977814@qq.com>
2021-11-09 20:30:36 +0800

    receive getMemberManageData request

---
+++ server/router/admin/memberManage.js
@@ -0,0 +1,19 @@
+module.exports = app => {
+    const express = require('express')
+    const router = express.Router()
+    const connect = require('../../plugins/connectMysql')
+    const {selectFields} = require('../../plugins/mysql_query')
+
+    router.get('/memberManage',(req, res) => {
+        const connection = connect()
+        const selectUser = selectFields('user','user_id,account,username,identity')
+        connection.query(selectUser, (err,result) => {
+            if (err) throw err
+            else {
+                res.send(result)
+            }
+        })
+    })
+
+    app.use('/admin',router)
+}

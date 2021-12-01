03009c397e48ffc00152366041eb5e4a6bf70462
zchengfa <1522977814@qq.com>
2021-12-01 12:07:15 +0800

    add routing guard

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -13,6 +13,28 @@ app.use(ElementPlus)
 //安装router
 app.use(router)
 
-
+//全局的路由守卫，进入所有路由前，检测是否需要登录后才能进入
+router.beforeEach((to,from,next)=>{
+    //若需要登录后进入，则判断是否有token值
+    if (to.meta.requireAuth){
+        //有token，表示已经登录过，直接进入
+        if (sessionStorage.getItem('token')){
+            next()
+        }
+        //没有token，表示未登录，重定向到登录页
+        else {
+            next({
+                path:'/login',
+                query:{
+                    redirect:from.fullPath
+                }
+            })
+        }
+    }
+    //无需登录校验，直接进入
+    else {
+        next()
+    }
+})
 
 app.mount('#app')
--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -1,17 +1,28 @@
 import {createRouter,createWebHistory} from "vue-router";
 
+const admin = () => import('../views/Admin.vue')
+const login = () => import('../components/Login.vue')
+const resetPassword = () => import('../components/ResetPassword.vue')
+
 const routes = [
     {
         path:'/',
-        component:() => import('../views/Admin.vue')
+        redirect:'/admin'
+    },
+    {
+        path: '/admin',
+        component:admin,
+        meta:{
+            requireAuth:true //进入管理页面需要进行登录验证
+        }
     },
     {
         path:'/login',
-        component:() => import('../components/Login.vue')
+        component: login
     },
     {
         path: '/resetPassword',
-        component:() => import('../components/ResetPassword.vue')
+        component: resetPassword
     }
 ]
 

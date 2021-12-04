a8ec69524f591ca8646bb71d198cf1c307d105eb
zchengfa <1522977814@qq.com>
2021-12-04 11:56:21 +0800

    add useUnifiedTopology:true and useNewUrlParser:true to correct waring

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,7 +1,7 @@
 //导入mongoose
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/mall', {useNewUrlParser:true,useUnifiedTopology:true},err => {
+mongoose.connect('mongodb://localhost:27017/mall', {useUnifiedTopology:true,useNewUrlParser:true},err => {
     if(err){
         console.log(err)
     }

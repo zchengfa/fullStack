2e2ce6d84a16a71b2e465270bf6bdeffef9d1fb0
zchengfa <1522977814@qq.com>
2021-12-04 11:51:28 +0800

    alter register page code

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,7 +1,7 @@
 //导入mongoose
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/mall', err => {
+mongoose.connect('mongodb://localhost:27017/mall', {useNewUrlParser:true,useUnifiedTopology:true},err => {
     if(err){
         console.log(err)
     }

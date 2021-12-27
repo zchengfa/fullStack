42168c8006bbb52ecc03c584ae4dcc6abaaa1c6a
zchengfa <1522977814@qq.com>
2021-12-27 11:41:44 +0800

    use sass

--- admin/src/components/ResetPassword.vue
+++ admin/src/components/ResetPassword.vue
@@ -1,5 +1,19 @@
 <template>
-<div>我是重置密码页面</div>
+<div class="mall-box">
+  <span>r</span>
+  <span>e</span>
+  <span>s</span>
+  <span>e</span>
+  <span>t</span>
+  <span>p</span>
+  <span>a</span>
+  <span>s</span>
+  <span>s</span>
+  <span>w</span>
+  <span>o</span>
+  <span>r</span>
+  <span>d</span>
+</div>
 </template>
 
 <script>
@@ -8,6 +22,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+$black: #000
+$begin:1
+$end:13
+.mall-box
+  width: 100vw
+  height: 100vh
+  background-color: $black
+  span
+    display: inline-block
+    font-family: "Rage Italic", serif
+    font-size: 10rem
+    text-shadow: 0 0 0 whitesmoke
+    animation: smoky 5s
+
+@keyframes smoky
+  to
+    transform: translateX(20rem) translateY(-5rem) rotateY(-40deg) skew(70deg) scale(1.5)
+    text-shadow: 0 0 20rem whitesmoke
+    opacity: 0
+
+@for $item from $begin through $end
+  span:nth-of-type(#{$item})
+    animation-delay: #{$item}/(#{$end}/2)s
 
 </style>

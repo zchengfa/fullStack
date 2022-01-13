531cfdf30fc1a183786255318fc5ac5221ff17a5
zchengfa <1522977814@qq.com>
2022-01-13 12:14:34 +0800

    finish:change cart params logic

---
+++ web/src/components/content/changeSize/ChangeSize.vue
@@ -0,0 +1,96 @@
+<template>
+<div class="change-size">
+  <div class="product-box" >
+    <div class="product-image"><img :src="productImage" alt="productImage"></div>
+    <product-size class="product-size-component" :item-size="item"></product-size>
+    <div class="submit-change-size"><button @click="submitChangeSize">确定</button></div>
+  </div>
+</div>
+</template>
+
+<script>
+import ProductSize from "@/views/detail/component/content/ProductSize";
+export default {
+  name: "ChangeSize",
+  props:{
+    productImage:{
+      type:String,
+      default(){
+        return ''
+      }
+    },
+    item:{
+      type:String,
+      default() {
+        return "";
+      }
+    }
+  },
+  data(){
+    return {
+      size:'',
+      sizeIndex:-1
+    }
+  },
+  components:{
+    ProductSize
+  },
+  methods:{
+    submitChangeSize(){
+      if (this.size || this.sizeIndex >=0){
+        let size = this.size
+        this.$bus.$emit('submitChangeSize',size)
+      }
+      else {
+        this.$toast.showToast('请选择尺码')
+      }
+
+    }
+  },
+  created() {
+    this.size = this.item
+  },
+  mounted() {
+    this.$bus.$on('choseSize',(e)=>{
+      this.size = e.item
+      this.sizeIndex = e.index
+    })
+  }
+}
+</script>
+
+<style scoped>
+.change-size{
+  width: 100%;
+  height: 100%;
+}
+.product-box{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 45%;
+  background-color: #fff;
+}
+.product-image{
+  width: 6rem;
+  margin-top: -3rem;
+  margin-left: 2rem;
+  border: 2px solid #fd001e;
+}
+.product-image img{
+  width: 100%;
+}
+.product-size-component,.submit-change-size{
+  margin-top: 10%;
+}
+.submit-change-size{
+  text-align: center;
+}
+.submit-change-size button{
+  width: 70%;
+  height: 2rem;
+  background-color: #fd001e;
+  color: #fff;
+  border-radius: 1rem;
+}
+</style>

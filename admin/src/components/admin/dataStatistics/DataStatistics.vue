8e913d56e05131ee9e2ad84facc08a2533cbe99b
zchengfa <1522977814@qq.com>
2022-04-27 12:05:27 +0800

    add rank

--- admin/src/components/admin/dataStatistics/DataStatistics.vue
+++ admin/src/components/admin/dataStatistics/DataStatistics.vue
@@ -67,7 +67,34 @@
       <UserLocationChartStatistics></UserLocationChartStatistics>
     </div>
   </div>
-  <div class="rank"></div>
+  <div class="rank">
+    <div class="gold-user-rank">
+      <h5>用户消费排行榜</h5>
+      <div class="title">
+        <span>排名</span>
+        <span>用户</span>
+        <span>地区</span>
+        <span>消费</span>
+      </div>
+    </div>
+    <div class="product-sales-rank">
+      <h5>商品销量排行榜</h5>
+      <div class="title">
+        <span>排名</span>
+        <span>商品类型</span>
+        <span>销量</span>
+        <span>总金额</span>
+      </div>
+    </div>
+    <div class="keyword-search-rank">
+      <h5>关键词搜索排行榜</h5>
+      <div class="title">
+        <span>排名</span>
+        <span>关键词</span>
+        <span>搜索次数</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -230,6 +257,39 @@ export default defineComponent({
   float: right;
   width: 20%;
   height: 100%;
-  background-color: #9ca9b6;
+  background-color: #fff;
+  overflow-y: scroll;
+}
+.rank div{
+  margin: 0 auto 2vh;
+  width: 96%;
+  height: 50%;
+  background-color: #cecfd0;
+  overflow-y: scroll;
+}
+.rank h5{
+  margin: 0 auto;
+  padding: 4%;
+  background-color: rgb(242, 236, 247);
+}
+.rank::-webkit-scrollbar,
+.rank div::-webkit-scrollbar{
+  display: none;
+}
+.rank .title{
+  width: 100%;
+  height: 10%;
+  min-height: 32px;
+  background-color: #fff;
+}
+.title span{
+  position: relative;
+  top: 50%;
+  display: inline-block;
+  width: 25%;
+  font-size: xx-small;
+  font-weight: bold;
+  text-align: center;
+  transform: translateY(-50%);
 }
 </style>

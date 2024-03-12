ecfd4242b618e0b846c90abf0204638c3c917bc9
zchengfa <1522977814@qq.com>
2024-03-12 19:28:37 +0800

    debug

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,66 +1,69 @@
 module.exports = {
-	configureWebpack:{
-		resolve:{
-			alias:{
-				'assets':'@/assets',
-				'common':'@/common',
-				'components':'@/components',
-				'views':'@/views',
-				'network':'@/network'
-			}
-		},
-		// rules:[
-		// 	//图片压缩（性能优化）
-		// 	{
-		// 		test: /\.(gif|png|jpe?g|svg)$/i,
-		// 		use:[
-		// 			{
-		// 				loader:'file-loader',
-		// 				options:{
-		// 					name:'[name].[hash:7].[ext]'
-		// 				}
-		// 			},
-		// 			{
-		// 				loader:'image-webpack-loader',
-		// 				options: {
-		// 					mozjpeg:{
-		// 						progressive:true,
-		// 						quality:50,
-		// 					},
-		// 					optipng:{
-		// 						enabled:true
-		// 					},
-		// 					panquant:{
-		// 						quality: [0.5,0.65],
-		// 						speed:4
-		// 					},
-		// 					gifsicle:{
-		// 						interlaced:false
-		// 					},
-		// 					webp:{
-		// 						quality:75
-		// 					}
-		// 				}
-		// 			}
-		// 		]
-		// 	}
-		//]
-	},
-	//关闭eslint检测
-	lintOnSave:false,
-	publicPath:'/',
-	//设置代理解决跨域问题
-	devServer:{
-		proxy:{
-			'/api':{
-				target:'http://apis.juhe.cn/fapig/douyin',
-				changeOrigin:true,
-				pathRewrite:{
-					'^/api':''
-				}
-			}
-		}
-	}
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'views': '@/views',
+        'network': '@/network'
+      }
+    },
+    //图片压缩
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.(png|jpe?g|gif|webp)$/i,
+    //       use: [
+    //         {
+    //           loader: 'url-loader',
+    //           options: {
+    //             limit: 10000,
+    //             name: 'img/[name].[hash:8].[ext]',
+    //           },
+    //         },
+    //         {
+    //           loader: 'image-webpack-loader',
+    //           options: {
+    //             mozjpeg: {
+    //               progressive: true,
+    //               quality: 65,
+    //             },
+    //             optipng: {
+    //               enabled: false,
+    //             },
+    //             pngquant: {
+    //               quality: [0.65, 0.9],
+    //               speed: 4,
+    //             },
+    //             gifsicle: {
+    //               interlaced: false,
+    //             },
+    //             webp: {
+    //               quality: 75,
+    //             },
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+  },
+  //关闭eslint检测
+  lintOnSave: false,
+  publicPath: '/',
+  //设置代理解决跨域问题
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://apis.juhe.cn/fapig/douyin',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 
 
 }

ce74360993e1971b458d05908f691ac74e8a98da
zchengfa <1522977814@qq.com>
2025-08-02 19:25:47 +0800

    perf(优化axios的二次封装代码):

--- admin/src/network/network.ts
+++ admin/src/network/network.ts
@@ -9,84 +9,60 @@ let baseURL = URL + '/admin'
 let timeout = 5000
 let requestNum = 0
 
+const instance = axios.create({baseURL,timeout})
+
 const hideLoadingDebounce = debounce(() => {
     if(store.isLoading){
         store.changeLoadingStatus(false)
     }
-})
+},500)
 
 function hideLoading (){
-    requestNum--
-    if(requestNum <= 0){
-        if(store.isLoading){
-            store.changeLoadingStatus(false)
-        }
+    if(requestNum <= 0 && store.isLoading){
         hideLoadingDebounce()
     }
 }
 
+//axios请求拦截器
+instance.interceptors.request.use(function (config:AxiosRequestConfig){
+    if (sessionStorage.getItem('token')){
+        config.headers.authorization = sessionStorage.getItem('token')
+    }
+    if(!store.isLoading && config.headers?.showLoading != false){
+        store.changeLoadingStatus(true)
+        requestNum ++
+    }
+    return config
+}, (err:any) =>{
+    hideLoading();
+    return Promise.reject(err)
+})
+
+//axios响应拦截器
+instance.interceptors.response.use((response:AxiosResponse) =>{
+    if (response.data.err === 401){
+        sessionStorage.removeItem('token')
+        router.push('/login').then()
+    }
+    requestNum--
+    hideLoading()
+    return response
+}, (err:any) => {
+    requestNum--
+    hideLoading()
+    return Promise.reject(err)
+})
+
 export function Get(config:Object){
-    const instance = axios.create({
-        baseURL,
-        timeout
-    })
-
-    axiosInterceptors(instance)
-
-    return instance(config)
+    return instance({...config,method:'GET'})
 }
 
 export function Post (config:Object){
-    const instance = axios.create({
-        baseURL,
-        method:"post",
-        timeout
-    })
-
-   axiosInterceptors(instance)
-
-    return instance(config)
+    return instance({...config,method:'POST'})
 }
 
 export function Delete (config:Object){
-    const instance = axios.create({
-        baseURL,
-        method:"delete",
-        timeout
-    })
-
-    axiosInterceptors(instance)
-
-    return instance(config)
+    return instance({...config,method:'DELETE'})
 }
 
-function axiosInterceptors(instance:any){
-    //axios请求拦截器
-    instance.interceptors.request.use(function (config:AxiosRequestConfig){
-        if (sessionStorage.getItem('token')){
-            config.headers.authorization = sessionStorage.getItem('token')
-        }
-        if(!store.isLoading && config.headers?.showLoading != false){
-            store.changeLoadingStatus(true)
-            requestNum ++
-        }
-        return config
-    }, (err:any) =>{
-        hideLoading();
-        return Promise.reject(err)
-    })
-
-    //axios响应拦截器
-    instance.interceptors.response.use((response:AxiosResponse) =>{
-        if (response.data.err === 401){
-            router.push('/login').then()
-        }
-
-        hideLoading()
-        return response
-    }, (err:any) => {
-        hideLoading()
-        return Promise.reject(err)
-    })
-}
 
